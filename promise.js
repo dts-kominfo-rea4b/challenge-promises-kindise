bfd6905ee31c6b9e47948f11beb8da39aca1c671
senaacls <kinsetiadi@gmail.com>
2022-09-18 21:24:55 +0700

    latihan promise

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  try {
+    const reactionsOnIXX = await promiseTheaterIXX();
+    const reactionsOnVGC = await promiseTheaterVGC();
+    const reactionsAfterWatchingBothTheaters = [
+      ...reactionsOnIXX,
+      ...reactionsOnVGC
+    ];
+    return reactionsAfterWatchingBothTheaters.filter(
+      (result) => result.hasil === emotion
+    ).length;
+  } catch (error) {
+    return "Err: " + error.message;
+  }
+}
 
 module.exports = {
   promiseOutput,
